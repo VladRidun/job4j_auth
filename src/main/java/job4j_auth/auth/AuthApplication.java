7fd9ec40f3161e273a9c599127b188f2263c8694
VladRidun <ridunvv@gmail.com>
2024-06-18 11:16:38 +0400

    start rest

---
+++ src/main/java/job4j_auth/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package job4j_auth.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/job4j_auth/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package job4j_auth.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
