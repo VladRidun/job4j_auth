521b756df18fb344a8f3d24927cf205e0ed265a4
VladRidun <ridunvv@gmail.com>
2024-06-25 09:11:48 +0400

    1. Spring Liquibase [#302330]

--- src/main/resources/db/002_dml_insert_person_table.sql
+++ src/main/resources/db/002_dml_insert_person_table.sql
@@ -1,9 +1,3 @@
-create table person (
-                        id serial primary key not null,
-                        login varchar(2000) unique,
-                        password varchar(2000)
-);
-
 insert into person (login, password) values ('Vlad', '123');
 insert into person (login, password) values ('Marina', '123');
 insert into person (login, password) values ('Artem', '123');
